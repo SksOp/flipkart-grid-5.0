094bf4af5fa535a6cefb4b57d06299fa5430b153
tanish1403 <tanish.jdh2020@gmail.com>
2023-08-19 23:21:20 +0530

    entry_adder_update

--- ui/entry_adder.py
+++ ui/entry_adder.py
@@ -1,35 +1,44 @@
 import streamlit as st
+from utils.model import agent,tools,memory,msgs
 import pandas as pd
 
 
-def take_entrie():
-    # st.set_page_config(layout="wide")
-    st.divider()
 
+def take_entrie():
+    st.divider()
     st.header("Custom purchase History")
     col1, col2 = st.columns(2)
     with col1:
         product = st.text_input("Enter the product name")
+
     with col2:
         price = st.text_input("Enter the price")
+
     session_state = get_session_state()
-    col1, col2 = st.columns(2)
-
-    def callback_to_add_entry():
-        add_entry(session_state, product, price)
-
-    def callback_to_clear_entries():
-        session_state.entries = []
-
+    col1, col2 = st.columns(2)   
+    
     with col1:
-        st.button("➕ Add", key="add_entry", on_click=callback_to_add_entry)
+        if st.button("➕ Add", key="add_entry"):
+            try:    
+                if product!="" and (price!="" and type(int(price))==int):
+                    add_entry(session_state, product, int(price))
+                
+                   
+                else:
+                    st.write('<span style="color:red;">You are leaving space blank or enter the wrong price</span>', unsafe_allow_html=True)
+            except:
+                st.write('<span style="color:red;">Please enter the price as an integer</span>', unsafe_allow_html=True)
     with col2:
-        st.button("❌ Clear", key="clear", on_click=callback_to_clear_entries)
-
-    # Display the table of key-value pairs
+        if st.button("❌ Clear", key="clear"):
+            session_state.entries = []
     display_entries(session_state)
-    st.divider()
 
+
+    
+
+    
+    # Display the table of key-value pairs
+    
 
 def get_session_state():
     # Create a session state object if it doesn't exist
@@ -37,21 +46,22 @@ def get_session_state():
         st.session_state.entries = []
     return st.session_state
 
-
 def add_entry(session_state, key, value):
     # Append the new key-value pair as a dictionary to the session state
     entry = {"product_name": key, "price": value}
     session_state.entries.append(entry)
 
-
 def display_entries(session_state):
     get_session_state()
     # Display the table using st.table
-    df = pd.DataFrame(session_state.entries)
+    df = pd.DataFrame(session_state.entries, columns=["product_name", "price"])
+    df.columns = ["Product Name", "Price"]
     id = len(df)
     # print([i+1 for i in range(id)])
     ind = [i+1 for i in range(id)]
-    df.index = ind
+    df.index=ind
     # Display the DataFrame using st.dataframe
     if not df.empty:
         st.dataframe(df)
+    product = st.empty()
+    price = st.empty()
